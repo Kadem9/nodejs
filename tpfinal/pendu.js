3fb4308a5621dfd6ceaf5800e5373d82fa02585a
Kadem Garnier <kadem.garnier@gmail.com>
2022-12-22 09:49:32 +0100

    start pendu

---
+++ tpfinal/pendu.js
@@ -0,0 +1,20 @@
+// Nous allons stocker dans une variable le mot à trouver
+const mot = "Developpement";
+// Nous allons stocker dans une variable le nombre de tentatives
+const nbTentatives = 10;
+// Nous allons stocker dans une variable le nombre de lettres trouvées
+let nbLettresTrouvees = 0;
+// Nous allons stocker dans une variable le nombre de tentatives restantes
+let nbTentativesRestantes = nbTentatives;
+// Nous allons stocker dans une variable le mot à trouver avec des tirets
+let motCache = "";
+// Nous allons stocker dans une variable les lettres déjà proposées
+let lettresProposees = "";
+// Nous allons stocker dans une variable le mot à trouver avec des tirets
+for (let i = 0; i < mot.length; i++) {
+    motCache += "-";
+}
+// Nous allons stocker dans une variable le mot à trouver avec des tirets
+for (let i = 0; i < mot.length; i++) {
+    motCache += "-";
+}
